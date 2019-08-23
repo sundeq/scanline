00f2ea2f7c3c5362dfc80b1d2b87d7156adfa0f5
Martin Sundeqvist <martin.sundequist@gmail.com>
2019-08-24 01:32:37 +0200

    (Seemingly) finished quicksort

--- scanline.c
+++ scanline.c
@@ -19,5 +19,20 @@ void edge_list_quick_sort(Edge **edge_list, int start_i, int end_i) {
 }
 
 int edge_list_partition(Edge **edge_list, int start_i, int end_i) {
-    return 1;
+    Edge *pivot = edge_list[end_i];
+    Edge *temp;
+    int i = start_i;
+    int j;
+    for (j = start_i; j < end_i; j++) {
+        if (edge_list[j]->x <= pivot->x) {
+            i++;
+            temp = init_edge(edge_list[j]->y_max, edge_list[j]->x, edge_list[j]->inv_m);
+            edge_list[j] = edge_list[i];
+            edge_list[i] = temp;
+        }
+    }
+    temp = init_edge(edge_list[i + 1]->y_max, edge_list[i + 1]->x, edge_list[i + 1]->inv_m);
+    edge_list[i + 1] = edge_list[end_i];
+    edge_list[end_i] = temp;
+    return i;
 }
